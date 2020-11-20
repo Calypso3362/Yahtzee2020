ac7187b8bfe10a889f1f38fa643e8bfbc45744c1
thomaschan123 <71958899+thomaschan123@users.noreply.github.com>
2020-11-21 01:05:43 +0800

    Added some functions and output change
    Added some functions and change the game into 2 people.
    Some output is changed

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <ctime>
+#include <string>
 
 using namespace std;
 
@@ -7,15 +9,25 @@ string second_combination[7]={"3 of a Kind","4 of a Kind","Full House","Small St
 
 int main()
 {
-    cout<<"Welcome to the Yahtzee, players!"<<endl<<"We have prepared two gamemodes for you!!!"<<endl<<"To enjoy normal mode, please enter 'n' as gamemode afterward."<<endl<<"To enjoy creative mode, please enter 'c' as gamemode afterward."<<endl<<"Please enter the number of players and the gamemode in the format as (number_of_players gamemode): ";
-    int player;
+    string player1, player2;
+    cout<<"Please input your name, player1."<<endl;
+    cin>>player1;
+    cout<<"Please input your name, player2."<<endl;
+    cin>>player2;
+    cout<<"Welcome to the Yahtzee, "<<player1<<" and "<<player2<<" !"<<endl<<"We have prepared two gamemodes for you!!!"<<endl<<"To enjoy normal mode, please enter 'n' as gamemode afterward."<<endl<<"To enjoy creative mode, please enter 'c' as gamemode afterward."<<endl<<"Please enter the gamemode: ";
     char mode;
-    cin>>player>>mode;
+    cin>>mode;
+
+    
+    
     
     //game start functions//
+    
+    
     
     //count score function//
     
     //winner//
-    
+    cout<<"Congratulations "<<player1<<" You won!"
+        
 }
