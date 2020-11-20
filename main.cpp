1e2eb46241aa4986a138df04f0d794740ca636ec
Calypso3362 <71886928+Calypso3362@users.noreply.github.com>
2020-11-20 20:22:40 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+string first_combination[6]={"Aces","Twos","Threes","Fours","Fives","Sixes"};
+string second_combination[7]={"3 of a Kind","4 of a Kind","Full House","Small Straight","Large Straight","YAHTZEE"," Chance"};
+
+int main()
+{
+    cout<<"Welcome to the Yahtzee, players!"<<endl<<"We have prepared two gamemodes for you!!!"<<endl<<"To enjoy normal mode, please enter 'n' as gamemode afterward."<<endl<<"To enjoy creative mode, please enter 'c' as gamemode afterward."<<endl<<"Please enter the number of players and the gamemode in the format as (number_of_players gamemode): ";
+    int player;
+    char mode;
+    cin>>player>>mode;
+    
+    //game start functions//
+    
+    //count score function//
+    
+    //winner//
+    
+}
